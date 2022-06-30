1d36f3e2874121559fef8e4be58bdd0ed551ea4b
Mirei124 <3100496917@qq.com>
2022-07-01 00:23:18 +0800

    1. some fix

--- config/polybar/forest/launch.sh
+++ config/polybar/forest/launch.sh
@@ -11,8 +11,8 @@ fixModule() {
 
 polybarStart() {
     # Terminate already running bar instances
-    # killall -q polybar
-    polybar-msg cmd quit
+    killall -q polybar
+    # polybar-msg cmd quit
 
     # Wait until the processes have been shut down
     while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
